01e2ce43682762ebc3518eabf45c453bcbf26ee9
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2021-10-08 09:34:59 +0300

    first parser

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,5 @@
 pub struct Program {
-    decls: Vec<Decl>,
+    pub decls: Vec<Decl>,
 }
 
 pub enum Decl {
@@ -10,19 +10,19 @@ pub enum Decl {
 }
 
 pub struct StructDecl {
-    ident: String,
-    fns: Vec<FunDecl>,
+    pub ident: String,
+    pub fns: Vec<FunDecl>,
 }
 
 pub struct FunDecl {
-    ident: String,
-    params: Vec<String>,
-    blk: Block,
+    pub ident: String,
+    pub params: Vec<String>,
+    pub blk: Block,
 }
 
 pub struct VarDecl {
-    ident: String,
-    expr: Expr,
+    pub ident: String,
+    pub expr: Expr,
 }
 
 pub enum Stmt {
@@ -33,37 +33,48 @@ pub enum Stmt {
 }
 
 pub struct Assignment {
-    call: Call,
-    expr: Expr,
+    pub call: Call,
+    pub op: AssignOp,
+    pub expr: Expr,
+}
+
+pub enum AssignOp {
+    Assign,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    BitAnd,
+    BitOr,
+    BitXor,
+    LeftShift,
+    RightShift,
+    LogicAnd,
+    LogicOr,
 }
 
 pub struct Expr {
-    or: LogicOr,
+    pub or: LogicOr,
 }
 
 pub enum LogicOr {
     A(LogicAnd),
-    B {
-        left: LogicOr,
-        right: LogicAnd,
-    }
+    B { left: Box<LogicOr>, right: LogicAnd },
 }
 
 pub enum LogicAnd {
     A(Cmp),
-    B {
-        left: LogicAnd,
-        right: Cmp,
-    }
+    B { left: Box<LogicAnd>, right: Cmp },
 }
 
 pub enum Cmp {
     A(BitOr),
     B {
-        left: Cmp,
+        left: Box<Cmp>,
         op: CmpOp,
         right: BitOr,
-    }
+    },
 }
 
 pub enum CmpOp {
@@ -77,35 +88,26 @@ pub enum CmpOp {
 
 pub enum BitOr {
     A(BitXor),
-    B {
-        left: BitOr,
-        right: BitXor,
-    }
+    B { left: Box<BitOr>, right: BitXor },
 }
 
 pub enum BitXor {
     A(BitAnd),
-    B {
-        left: BitXor,
-        right: BitAnd,
-    }
+    B { left: Box<BitXor>, right: BitAnd },
 }
 
 pub enum BitAnd {
     A(Shift),
-    B {
-        left: BitAnd,
-        right: Shift,
-    }
+    B { left: Box<BitAnd>, right: Shift },
 }
 
 pub enum Shift {
     A(Term),
     B {
-        left: Shift,
+        left: Box<Shift>,
         op: ShiftOp,
         right: Term,
-    }
+    },
 }
 
 pub enum ShiftOp {
@@ -116,10 +118,10 @@ pub enum ShiftOp {
 pub enum Term {
     A(Factor),
     B {
-        left: Term,
+        left: Box<Term>,
         op: TermOp,
         right: Factor,
-    }
+    },
 }
 
 pub enum TermOp {
@@ -130,10 +132,10 @@ pub enum TermOp {
 pub enum Factor {
     A(TypeCast),
     B {
-        left: Factor,
+        left: Box<Factor>,
         op: FactorOp,
         right: TypeCast,
-    }
+    },
 }
 
 pub enum FactorOp {
@@ -145,9 +147,9 @@ pub enum FactorOp {
 pub enum TypeCast {
     A(Unary),
     B {
-        left: TypeCast,
+        left: Box<TypeCast>,
         type_name: TypeName,
-    }
+    },
 }
 
 pub enum TypeName {
@@ -162,4 +164,99 @@ pub enum TypeName {
 }
 
 pub enum Unary {
+    A(Call),
+    B { op: UnaryOp, unary: Box<Unary> },
+}
+
+pub enum UnaryOp {
+    Minus,
+    Not,
+}
+
+pub struct Call {
+    pub primary: Primary,
+    pub call: Vec<CallPart>,
+}
+
+pub enum CallPart {
+    Ident(String),
+    Brkts(Box<Expr>),
+    FunCall(Vec<Expr>),
+    QMark,
+}
+
+pub enum Primary {
+    Prnth(Box<Expr>),
+    Literal(Literal),
+    This,
+    Ident(String),
+    For(For),
+    While(While),
+    Loop(Loop),
+    If(If),
+    Block(Block),
+}
+
+pub struct For {
+    pub ident: String,
+    pub expr: Box<Expr>,
+    pub blk: Block,
+}
+
+pub struct While {
+    pub expr: Box<Expr>,
+    pub blk: Block,
+}
+
+pub struct Loop {
+    pub blk: Block,
+}
+
+pub struct If {
+    pub cond: Box<Expr>,
+    pub blk: Block,
+    pub els: Option<Else>,
+}
+
+pub enum Else {
+    Block(Block),
+    If(Box<If>),
+}
+
+pub enum Literal {
+    Bool(bool),
+    Null,
+    Int(i64),
+    Float(f64),
+    Str(String),
+    Char(char),
+    Map(Vec<MapField>),
+    Array(Vec<Expr>),
+    Struct(StructLit),
+    Closure(Closure),
+}
+
+pub struct MapField {
+    pub key: String,
+    pub val: Box<Expr>,
+}
+
+pub struct StructLit {
+    pub ident: String,
+    pub fields: Vec<StructField>,
+}
+
+pub struct StructField {
+    pub ident: String,
+    pub expr: Box<Expr>,
+}
+
+pub struct Closure {
+    pub params: Vec<String>,
+    pub blk: Block,
+}
+
+pub struct Block {
+    pub decls: Vec<Decl>,
+    pub expr: Box<Option<Expr>>,
 }
