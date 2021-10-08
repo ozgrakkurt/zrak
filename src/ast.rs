654b79387c098c914609b89e3ba35f9c0536a99b
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2021-10-08 09:12:52 +0300

    parser progress

--- src/ast.rs
+++ src/ast.rs
@@ -37,11 +37,129 @@ pub struct Assignment {
     expr: Expr,
 }
 
-pub enum Expr {
-    For(For),
-    While(While),
-    Loop(Loop),
-    If(If),
-    Block(Block),
-    LogicOr(LogicOr),
+pub struct Expr {
+    or: LogicOr,
+}
+
+pub enum LogicOr {
+    A(LogicAnd),
+    B {
+        left: LogicOr,
+        right: LogicAnd,
+    }
+}
+
+pub enum LogicAnd {
+    A(Cmp),
+    B {
+        left: LogicAnd,
+        right: Cmp,
+    }
+}
+
+pub enum Cmp {
+    A(BitOr),
+    B {
+        left: Cmp,
+        op: CmpOp,
+        right: BitOr,
+    }
+}
+
+pub enum CmpOp {
+    Eq,
+    NotEq,
+    Less,
+    Grt,
+    LessEq,
+    GrtEq,
+}
+
+pub enum BitOr {
+    A(BitXor),
+    B {
+        left: BitOr,
+        right: BitXor,
+    }
+}
+
+pub enum BitXor {
+    A(BitAnd),
+    B {
+        left: BitXor,
+        right: BitAnd,
+    }
+}
+
+pub enum BitAnd {
+    A(Shift),
+    B {
+        left: BitAnd,
+        right: Shift,
+    }
+}
+
+pub enum Shift {
+    A(Term),
+    B {
+        left: Shift,
+        op: ShiftOp,
+        right: Term,
+    }
+}
+
+pub enum ShiftOp {
+    Left,
+    Right,
+}
+
+pub enum Term {
+    A(Factor),
+    B {
+        left: Term,
+        op: TermOp,
+        right: Factor,
+    }
+}
+
+pub enum TermOp {
+    Add,
+    Sub,
+}
+
+pub enum Factor {
+    A(TypeCast),
+    B {
+        left: Factor,
+        op: FactorOp,
+        right: TypeCast,
+    }
+}
+
+pub enum FactorOp {
+    Mul,
+    Div,
+    Mod,
+}
+
+pub enum TypeCast {
+    A(Unary),
+    B {
+        left: TypeCast,
+        type_name: TypeName,
+    }
+}
+
+pub enum TypeName {
+    Bool,
+    Int,
+    Float,
+    Function,
+    Closure,
+    Map,
+    Array,
+    Ident(String),
+}
+
+pub enum Unary {
 }
