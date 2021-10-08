3de7977f84b205f7dbd023d9e317484e239d0e69
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2021-10-09 00:40:56 +0300

    remove lalrpop add grammar

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    lalrpop::Configuration::new()
-        .use_cargo_dir_conventions()
-        .process_file("src/grammar.lalrpop")
-        .unwrap();
-
-    println!("cargo:rerun-if-changed=src/grammar.lalrpop");
-}
