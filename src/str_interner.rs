d7fb7fe81130e3f397dfa2fc41e2e06c2392ca20
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2021-10-18 11:28:47 +0300

    simplify string interner more

--- src/str_interner.rs
+++ src/str_interner.rs
@@ -3,8 +3,7 @@ use std::mem;
 
 pub struct Interner {
     map: HashMap<&'static str, usize>,
-    vec: Vec<&'static str>,
-    buf: Vec<String>,
+    vec: Vec<String>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -15,26 +14,24 @@ impl Interner {
         Interner {
             map: HashMap::new(),
             vec: Vec::new(),
-            buf: Vec::new(),
         }
     }
 
-    pub fn intern(&mut self, s: &str) -> IntStr {
-        if let Some(&idx) = self.map.get(s) {
+    pub fn intern<S: Into<String>>(&mut self, s: S) -> IntStr {
+        let string = s.into();
+        if let Some(&idx) = self.map.get(string.as_str()) {
             return IntStr(idx);
         }
-        let string = s.to_owned();
         let s = unsafe { mem::transmute::<&str, &'static str>(&string) };
-        self.buf.push(string);
-        let idx = self.map.len();
+        let idx = self.vec.len();
+        self.vec.push(string);
         self.map.insert(s, idx);
-        self.vec.push(s);
 
         IntStr(idx)
     }
 
     pub fn lookup(&self, s: IntStr) -> Option<&str> {
-        self.vec.get(s.0).copied()
+        self.vec.get(s.0).map(String::as_str)
     }
 }
 
